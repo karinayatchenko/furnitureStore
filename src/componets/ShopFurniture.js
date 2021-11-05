08791ce7f1801db7369af7b2700bbb305199fb85
Karina Y <karina.yatchenko@hellofresh.com>
2021-11-05 17:12:17 -0400

    created redux store , added fakestore api

--- src/componets/ShopFurniture.js
+++ src/componets/ShopFurniture.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+import ProductComponent from "./ProductComponent";
 
 export const ShopFurniture = () => {
-  return <div>Shop furniture here today</div>;
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products:", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
 };
 
 export default ShopFurniture;
