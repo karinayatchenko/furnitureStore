2e4f48fb40503c90fd893df5fc37fc74b507d6a0
Karina Y <karina.yatchenko@hellofresh.com>
2021-11-18 13:21:22 -0500

    add single item view

--- src/componets/ProductComponent.js
+++ src/componets/ProductComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
@@ -6,19 +7,21 @@ const ProductComponent = () => {
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-      <div className="four column wide" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img scr={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price"> $ {price}</div>
-              <div className="meta"> {category}</div>
+      <div key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} size="40x40" />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price"> $ {price}</div>
+                <div className="meta"> {category}</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
